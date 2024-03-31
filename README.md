d9a5ae1469a5cbd356e985990111b85cebf9e14f
shaneforthun0729 <shaneforthun0729@gmail.com>
2024-03-31 18:05:34 +0800

    html files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 "# CIGENDFIRE" 
+
+
+
+
+#requirements
+--node js
+--firebase
+--create index.js in source to set up your firebase account
+
+
+
+
+
+
+
+#how to run
+--npm i 
+--npx webpack serve
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!(firebase)\/).*/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  resolve: {
+    fallback: {
+      fs: false,
+      path: require.resolve('path-browserify'),
+    },
+  },
+};
